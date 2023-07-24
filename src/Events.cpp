41be3698bed922252ee886e5270760b677467e76
Angad <angadmisra28@gmail.com>
2023-07-24 16:45:00 -0700

    Idk

--- src/Events.cpp
+++ src/Events.cpp
@@ -16,18 +16,24 @@ namespace Events {
         if (const auto actor = a_event->actor->As<RE::Actor>()) {
             if (!actor->IsPlayerRef() && actor->Is3DLoaded()) {
                 if (const auto item = RE::TESForm::LookupByID<RE::TESObjectARMO>(a_event->baseObject)) {
-                    if (item->GetSlotMask() == RE::BGSBipedObjectForm::BipedObjectSlot::kBody && "Underwear"sv.compare(item->GetName())) {
-                        std::random_device rd;
-                        std::mt19937 rng(rd());
-                        std::uniform_int_distribution dist(0, 3);
-                        const auto idx = dist(rng);
-                        const auto undie = Utility::underwear[idx];
-                        actor->AddObjectToContainer(undie, nullptr, 1, nullptr);
-                        const auto inv = actor->GetInventory([](RE::TESBoundObject& obj) { return obj.IsArmor(); });
-                        if (inv.contains(undie)) {
-                            const auto manager = RE::ActorEquipManager::GetSingleton();
-                            manager->EquipObject(actor, undie, nullptr, 1, nullptr,
-                                                 true, true, false, true);
+                    logger::info("Item: {}, EDID: {}", item->GetName(), item->GetFormEditorID());
+                    if (std::ranges::find(Utility::underwear, item) == Utility::underwear.end()) {
+                        if (item->GetSlotMask() == RE::BGSBipedObjectForm::BipedObjectSlot::kBody) {
+                            logger::info("Unequipped {} from {}", item->GetName(), actor->GetName());
+                            std::random_device rd;
+                            std::mt19937 rng(rd());
+                            std::uniform_int_distribution dist(0, 3);
+                            const auto idx = dist(rng);
+                            const auto undie = Utility::underwear[idx];
+                            actor->AddObjectToContainer(undie, nullptr, 1, nullptr);
+                            logger::info("Added {} to {}'s inventory", undie->GetName(), actor->GetName());
+                            const auto inv = actor->GetInventory([](RE::TESBoundObject& obj) { return obj.IsArmor(); }, true);
+                            if (inv.contains(undie)) {
+                                logger::info("{} has underwear in inventory", actor->GetName());
+                                const auto manager = RE::ActorEquipManager::GetSingleton();
+                                manager->EquipObject(actor, undie, nullptr, 1, nullptr, true, false, false, false);
+                                logger::info("Equipped underwear to {}", actor->GetName());
+                            }
                         }
                     }
                 }
